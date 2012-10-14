275bb91e4d7a1b1fdffdf67278fba0a53581df1f
t.fidecki@gmail.com <t.fidecki@gmail.com@476995c6-25d3-f0f9-365f-13fa6fcdef69>
2012-10-14 06:28:45 +0000

    Refactoring.

--- myHome_Actor8CH/myHome_Actor8CH/src/myHome_Comm.h
+++ myHome_Actor8CH/myHome_Actor8CH/src/myHome_Comm.h
@@ -9,12 +9,13 @@
 #define MYHOME_COMM_H_
 
 #include "compiler.h"
+#include "fifo.h"
 
 // Actor with 8 Relay Channels
 #define  A8CH_DATA_FRAME_SIZE	(8)
 
 // Device types
-#define A8CH_DEVICE_TYPE		(0x0000)
+#define A8CH_DEVICE_TYPE		(0x00)
 
 // Commands
 typedef enum
@@ -24,20 +25,20 @@ typedef enum
 	MYHOME_COMM_GROUPSET,
 	
 	MYHOME_COMM_MAX
-} MyHomeCommCommands;
+} MyHomeCommCommands_t;
 
 //! Data Frame descriptor.
 struct data_frame_desc
 {
-	uint8_t				u8DeviceType;
-	MyHomeCommCommands	eCommand;    
-	uint8_t				u8DataArray[6];
+	uint8_t					u8DeviceID;		// device type (higher 4b) and address (lower 4b)
+	MyHomeCommCommands_t	eCommand;		// command
+	uint8_t					u8DataArray[6];	// 6 bytes of data
 };
 
 typedef struct data_frame_desc data_frame_desc_t;
 
 void myHome_Comm_Init(data_frame_desc_t * a_pDataFrameDesc);
 
-void myHome_Comm_Data_Get(fifo_desc_t * a_pFifoDesc, data_frame_desc_t * a_pDataFrameDesc);
+void myHome_Comm_Data_CopyFrom(fifo_desc_t *a_pFifoDesc, data_frame_desc_t *a_pDataFrameDesc);
 
 #endif /* MYHOME_COMM_H_ */
