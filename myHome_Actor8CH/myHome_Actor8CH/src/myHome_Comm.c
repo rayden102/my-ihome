c1ebd2049935c569e5a2daa0e0c33172d2f6d5ed
t.fidecki@gmail.com <t.fidecki@gmail.com@476995c6-25d3-f0f9-365f-13fa6fcdef69>
2012-10-14 06:29:22 +0000

    Function myHome_Comm_Data_CopyFrom() added.

--- myHome_Actor8CH/myHome_Actor8CH/src/myHome_Comm.c
+++ myHome_Actor8CH/myHome_Actor8CH/src/myHome_Comm.c
@@ -12,7 +12,7 @@ void myHome_Comm_Init(data_frame_desc_t * a_pDataFrameDesc)
 	// Check provided arguments
 	Assert(NULL != a_pDataFrameDesc);
 	
-	a_pDataFrameDesc->u8DeviceType	 = 0;
+	a_pDataFrameDesc->u8DeviceID	 = 0;
 	a_pDataFrameDesc->eCommand		 = 0;
 	a_pDataFrameDesc->u8DataArray[0] = 0;
 	a_pDataFrameDesc->u8DataArray[1] = 0;
@@ -22,8 +22,41 @@ void myHome_Comm_Init(data_frame_desc_t * a_pDataFrameDesc)
 	a_pDataFrameDesc->u8DataArray[5] = 0;
 }; // myHome_Comm_Init()
 
-void myHome_Comm_Data_Get(fifo_desc_t * a_pFifoDesc, data_frame_desc_t * a_pDataFrameDesc)
+/**
+ *  \brief Copies 8 bytes of data from FIFO and
+ *		   puts into data frame descriptor. 
+ *
+ *  \param a_pFifoDesc		Pointer to the FIFO descriptor.
+ *  \param a_pDataFrameDesc Pointer to the data frame descriptor.
+ *
+ *  \return nothing
+ *    \retval nothing
+ */
+void myHome_Comm_Data_CopyFrom(fifo_desc_t *a_pFifoDesc, data_frame_desc_t *a_pDataFrameDesc)
 {
+	uint8_t __attribute__((unused)) u8FifoStatus;
+	uint8_t u8TmpData = 0;
 	
+	// Check provided arguments
+	Assert(NULL != a_pFifoDesc);
+	Assert(NULL != a_pDataFrameDesc);
 	
-} // myHome_Comm_Data_Get()
+	// Pull out first byte with device identifier (type and address)
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DeviceID));
+	
+	// Pull out byte with command
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &u8TmpData);
+	a_pDataFrameDesc->eCommand = (MyHomeCommCommands_t)u8TmpData;
+	
+	// Get 6 consecutive bytes
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[0]));
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[1]));
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[2]));
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[3]));
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[4]));
+	u8FifoStatus = fifo_pull_uint8(a_pFifoDesc, &(a_pDataFrameDesc->u8DataArray[5]));
+	
+	// We should be OK here
+	Assert(FIFO_OK == u8FifoStatus);
+	
+} // myHome_Comm_Data_Insert()
