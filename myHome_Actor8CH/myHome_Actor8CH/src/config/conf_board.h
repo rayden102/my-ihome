1437fc46ca1c1f65780ee8beb8fb03e9c24f454e
t.fidecki@gmail.com <t.fidecki@gmail.com@476995c6-25d3-f0f9-365f-13fa6fcdef69>
2012-10-23 18:48:06 +0000

    Initial delay timers handling added.

--- myHome_Actor8CH/myHome_Actor8CH/src/config/conf_board.h
+++ myHome_Actor8CH/myHome_Actor8CH/src/config/conf_board.h
@@ -17,6 +17,12 @@
 // To achieve 10Hz resolution the period value must be set to 3125
 #define TIMER_HEARTBEAT_PERIOD	(3125)
 
+// Heartbeat timer frequency
+#define SYSTEM_TICK_FREQ		(10)
+
+// Number of relay channels present on the board
+#define A8CH_RELAY_CHANNELS_COUNT	(8)
+
 // Enable USARTD0
 #define CONF_BOARD_ENABLE_USARTD0
 
